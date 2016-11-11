8772b5fb708113b97e6aef7acf7efab20c7e0075
BreahnaDavid <breahnadavid@gmail.com>
2016-11-11 19:28:42 +0200

    Force Https and Simple Logo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :force_https
+
+  def force_https
+    if !request.ssl? && force_https? && Rails.env.production?
+      redirect_to protocol: "https://", status: :moved_permanently
+    end
+  end
+
+  def force_https?
+    true
+  end
 end
